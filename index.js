b1f3aa3455570c3c4b3fc969f96cf030a0eea9fb
Felice Forby <feliceforby@gmail.com>
2018-12-24 23:07:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
-var customerName = 'bob';
+var customerName = 'bob'; // Do NOT use var normally
 
 function upperCaseCustomerName() {
   customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+  bestCustomer = 'not bob'; // NEVER declare a variable like this because it becomes global!
 }

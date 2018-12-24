f1719b0184d3993a1df8749fd1355df582667ed4
Felice Forby <feliceforby@gmail.com>
2018-12-24 23:15:45 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-/* Your code goes here */
+var customerName = 'bob'; // Do NOT use var normally
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+  // NEVER declare a variable like this because it becomes global! (see next function)
+  bestCustomer = 'not bob';
+}
+
+function overWriteBestCustomer() {
+  // overwrites the bestCustomer variable from above because it had been set as a global variable
+  bestCustomer = 'maybe bob';
+}
+
+const leastFavoriteCustomer = 'jim';
+
+function changeLeastFavoriteCustomer() {
+  // cannot reassign because the variable was set a constant
+  leastFavoriteCustomer = 'not jim';
+}
+
+function attemptTwoFavoriteCustomers() {
+  // will not work because using let ensures you cannot declare the same variable name twice
+  let favoriteCustomer = 'betty';
+  let favoriteCustomer = 'jiro';
+}

20272c8a71051df73ef6f07ee1dd310ac5c31934
Felice Forby <feliceforby@gmail.com>
2018-12-24 23:09:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,9 +5,13 @@ function upperCaseCustomerName() {
 }
 
 function setBestCustomer() {
-  bestCustomer = 'not bob'; // NEVER declare a variable like this because it becomes global!
+  // NEVER declare a variable like this because it becomes global! (see next function)
+  bestCustomer = 'not bob';
 }
 
 function overWriteBestCustomer() {
+  // overwrites the bestCustomer variable from above because it had been set as a global variable
   bestCustomer = 'maybe bob';
 }
+
+
